b479a9a961a06716c8fce9308ea33e0b38b2ee04
Wang Gang <jameswang6@gmail.com>
2016-03-30 21:37:05 +0800

    add a new method
    add method tell_year which returns 2016 hard coded

--- helloci/hello.py
+++ helloci/hello.py
@@ -1,4 +1,11 @@
+# -*-: coding: utf-8 -*-
+
 import sys
+
+
 def hello(name):
-    print u'Going to say hello to %(name)s' % { "name": name}
-    return name
+  print u'Going to say hello to %(name)s' % { "name": name}
+  return name
+
+def tell_year():
+  return 2016
